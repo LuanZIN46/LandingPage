7db7641a3e3d11a2aef0fdc8ed03654a492b64b1
Luan Christian <adilsomcotteoa123@gmail.com>
2026-05-23 16:35:40 -0300

    fix imports

--- meu/src/App.jsx
+++ meu/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import About from './components/About/About';
-import Skills from './components/Skills/Skills';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-import './App.css'
+import Header from './components/Header/Header.jsx';
+import Hero from './components/Hero/Hero.jsx';
+import About from './components/About/About.jsx';
+import Skills from './components/Skills/Skills.jsx';
+import Projects from './components/Projects/Projects.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import './App.css';
 
 function App() {
 
