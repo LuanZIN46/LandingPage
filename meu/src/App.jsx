af36b8719596b9eed08cf262edc88c147a459060
Luan Christian <adilsomcotteoa123@gmail.com>
2026-05-23 19:03:33 -0300

    fix skills import

--- meu/src/App.jsx
+++ meu/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Components/Header/Header.jsx';
 import Hero from './Components/Hero/Hero.jsx';
 import About from './Components/About/About.jsx';
-import Skills from '.Components/Skills/Skills.jsx';
+import Skills from './Components/Skills/Skills.jsx';
 import Projects from './Components/Projects/Projects.jsx';
 import Contact from './Components/Contact/Contact.jsx';
 import Footer from './Components/Footer/Footer.jsx';
